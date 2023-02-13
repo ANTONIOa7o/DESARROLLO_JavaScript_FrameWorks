020bf009ae9bb37876cf855fc8262c293989d0d5
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-02-13 20:04:42 +0100

    Add files via upload

---
+++ JavaScript_I/ejemplo3-Funciones_I/js/script3.js
@@ -0,0 +1,8 @@
+/*
+    Como métod su sintxis: .getElementById('nombreId');
+        Definido como LA PROYECCION DE UN ELEMENTO QUE CONTENDRÁ LA DEFINICIÓN QUE TRASLADA EL id.
+        En definitiva poder manipular u obtener informacion de un elemento y su contenido dentro del
+        DocType
+        id- referenciador que html para conectar al documento desde otros entornos
+
+*/
