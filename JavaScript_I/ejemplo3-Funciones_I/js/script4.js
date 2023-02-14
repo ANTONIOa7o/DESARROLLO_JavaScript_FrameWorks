4ca1b40dcd9800e5900b3c4ffaef7fe3473e26a4
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-02-14 20:05:36 +0100

    Add files via upload

---
+++ JavaScript_I/ejemplo3-Funciones_I/js/script4.js
@@ -0,0 +1,20 @@
+//1º Variables eje.
+var nombre;
+var primerApellido;
+var segundoApellido;
+var eMail;
+
+function autorizacion() {
+    //2º doy valor a las variables
+    nombre=document.getElementById('nombre').value;
+    primerApellido=document.getElementById('apellido').value;
+    eMail=document.getElementById('email').value;
+//3º Gestionaremos las variables
+
+
+//4º Muestra de las variables o sus interacciones
+document.getElementById('resultado').innerHTML=
+
+''
+    
+}
